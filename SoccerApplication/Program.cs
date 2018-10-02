74378dd3681eb92da3977180742041e750a2c206
Raphael <raphael.borro124294@student.leeuwenborgh.nl>
2018-10-02 12:35:54 +0200

    Db -> Database

--- SoccerApplication/Program.cs
+++ SoccerApplication/Program.cs
@@ -35,7 +35,7 @@ namespace SoccerApplication
                 catch (Exception ex)
                 {
                     var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred Initializing the DB.");
+                    logger.LogError(ex, "An error occurred Initializing the Database.");
                 }
             }
 
